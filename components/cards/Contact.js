d582ebbe04584f1ce403d63342d037a4ddcd0c14
0AmirKhan0 <amirabdolhosseinpour@gmail.com>
2024-08-21 18:41:33 +0330

    create contact form structure

--- components/cards/Contact.js
+++ components/cards/Contact.js
@@ -2,7 +2,7 @@ import Button from "../ui/Button";
 
 export default function ContactCard({ title, icon, text, btnText }) {
   return (
-    <div className="bg-primary-background border border-border rounded-lg relative overflow-hidden py-5 pl-[25px] shadow-md ">
+    <div className="bg-secondary-background border border-border rounded-lg relative overflow-hidden py-5 pl-[25px] shadow-md ">
       <div className="z-20 flex flex-col gap-8 justify-between items-start ">
         {/* header */}
         <div className="flex items-center gap-x-2">
--- sections/Contact.js
+++ sections/Contact.js
@@ -1,8 +1,12 @@
 import ContactCard from "@/components/cards/Contact";
 import Heading from "@/components/heading/Heading";
+import Button from "@/components/ui/Button";
 import Card from "@/components/ui/Card";
-import { FaPhoneVolume } from "react-icons/fa";
-import { MdEmail } from "react-icons/md";
+import Input from "@/components/ui/Input";
+import TextArea from "@/components/ui/TextArea";
+import { FaPhoneVolume, FaProjectDiagram, FaUser } from "react-icons/fa";
+import { MdEmail, MdSubject } from "react-icons/md";
+import { SiMinutemailer } from "react-icons/si";
 
 export default function ContactSection() {
   return (
@@ -26,6 +30,51 @@ export default function ContactSection() {
             />
           </div>
           {/* contact form */}
+          <div className="lg:col-span-2 bg-secondary-background border border-border rounded-lg space-y-6 relative overflow-hidden py-5 px-[25px] shadow-md">
+            <div className="flex flex-col lg:flex-row justify-between mb-8 gap-8">
+              <Input type="text" placeholder="Full Name" icon={<FaUser />} />
+              <Input
+                type="email"
+                placeholder="Email Address"
+                icon={<MdEmail />}
+              />
+            </div>
+            <div className="flex justify-between mb-8 gap-8">
+              <Input type="text" placeholder="Subject" icon={<MdSubject />} />
+            </div>
+            {/* multiple select wrapper */}
+            <div className="flex flex-col gap-6">
+              <div>
+                <h1 className="font-bold text-lg">
+                  What services are you in need for?
+                </h1>
+                <div className="flex flex-wrap items-center justify-between mb-4 gap-8">
+                  {/* services */}
+                </div>
+              </div>
+            </div>
+            {/* multiple select wrapper */}
+            <div className="flex flex-col gap-6">
+              <div>
+                <h1 className="font-bold text-lg">What is your budget ?</h1>
+                <div className="flex flex-wrap items-center justify-between mb-4 gap-8">
+                  {/* budget options */}
+                </div>
+              </div>
+            </div>
+            {/* textarea message */}
+
+            <TextArea
+              placeholder="Tell us about your project"
+              icon={<FaProjectDiagram />}
+            />
+            <div className="w-full flex justify-end">
+              <Button className="!w-44 !py-3 !text-xl">
+                <span>Send</span>
+                <SiMinutemailer />
+              </Button>
+            </div>
+          </div>
         </div>
       </Card>
     </div>
