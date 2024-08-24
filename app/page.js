1321132bd7f95afae6a42758c64bc51d8acd32b7
0AmirKhan0 <amirabdolhosseinpour@gmail.com>
2024-08-24 22:34:30 +0330

    uninstall water wave

--- app/page.js
+++ app/page.js
@@ -1,4 +1,3 @@
-import WaterWaveWrapper from "@/components/visualEffects/WaterWaveWrapper";
 import AboutSection from "@/sections/About";
 import ContactSection from "@/sections/Contact";
 import FeaturedSection from "@/sections/Featured";
@@ -6,20 +5,11 @@ import LandingSection from "@/sections/Landing";
 
 export default function Home() {
   return (
-    // <WaterWaveWrapper
-    //   imageUrl=""
-    //   dropRadius="3"
-    //   perturbance="3"
-    //   resolution="2048"
-    // >
-
     <div className="pb-8">
       <LandingSection />
       <FeaturedSection />
       <AboutSection />
       <ContactSection />
     </div>
-
-    // </WaterWaveWrapper>
   );
 }
